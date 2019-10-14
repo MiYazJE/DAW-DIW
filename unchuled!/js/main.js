cad92682c76888dbae222bfd0f3cc1c31c2ac6bd
MiYazJE <ruben.saizserrano@gmail.com>
2019-10-15 00:15:46 +0200

    Objetos Celda y Caja eliminados + movimientos del personaje

--- unchuled!/js/main.js
+++ unchuled!/js/main.js
@@ -1,77 +1,81 @@
-function Celda() {
-    this.estilo = 'celda';
-}
+window.addEventListener('load', () => {
 
-function Caja() {
-    this.estilo = 'caja';
-}
+    let mapa  = [];
+    let cajas = [];
 
-function Personaje() {
-    this.posX = 0;
-    this.posY = 0;
-    this.estilo = 'personaje';
-}
+    // Creación del mapa con las celdas
+    inicializarMapa();
+    // Recoger en un matriz todos los divs creados en la funcion inicializar mapa
+    recogerCajas();
+    cajas[0][0].classList.add('personaje');
 
-function Tablero() {
-    this.mapa = [];
-}
+    let personaje = {};
+    personaje.x = 0;
+    personaje.y = 0;
+    personaje.move = function (Y, X) {
+        let posX = personaje.x + X;
+        let posY = personaje.y + Y;
 
-function moverse(personaje, x, y) {
+        // Salir si la posicion del personaje esta fuera del mapa o si su direccion esta 
+        // ocupada por una caja
+        if (posX == mapa[0].length || posX == -1 || posY == mapa.length || posY == -1) return;
+        if (mapa[posY][posX] == 'caja') return;
 
-}
+        // Eliminar anterior posicion del personaje
+        mapa[personaje.y][personaje.x]  = 'celda';
+        cajas[personaje.y][personaje.x].classList.remove('personaje');
 
-function inicializarMapa() {
-    let mapa = [];
-    for (let i = 0; i < 13; i++) {
-        mapa[i] = new Array(21).fill(new Celda());
+        personaje.x = posX;
+        personaje.y = posY;
+
+        // Actualizar la nueva posicion del personaje en el mapa
+        mapa[personaje.y][personaje.x] = 'personaje';
+        cajas[personaje.y][personaje.x].classList.add('personaje');
     }
-    return mapa;
-}
 
-function removeChildren(node) {
-    while (node.firstChild) {
-        node.removeChild(node.firstChild);
+    function inicializarMapa() {
+
+        for (let i = 0; i < 13; i++)
+            mapa[i] = new Array(21).fill('celda');
+
+        let tablero = document.querySelector('.tablero');
+
+        for (let i = 0; i < mapa.length; i++) {
+            for (let j = 0; j < mapa[0].length; j++) {
+
+                let divCelda = document.createElement('div');
+
+                if (i % 3 == 0 || i == 0 || j == 0 || j % 4 == 0) {
+                    mapa[i][j] = 'celda';
+                }
+                else {
+                    mapa[i][j] = 'caja';
+                }
+
+                divCelda.classList.add(mapa[i][j]);
+                tablero.appendChild(divCelda);
+            }
+        }
+
+        mapa[0][0] = 'personaje';
     }
-}
 
-function pintarTablero(mapa) {
-    let tablero = document.querySelector('.tablero');
-    removeChildren(tablero);
-    for (let i = 0; i < mapa.length; i++) {
-        for (let j = 0; j < mapa[0].length; j++) {
-            let divCelda = document.createElement('div');
-            if (mapa[i][j] instanceof Personaje) {
-                
-            }
-            else if (i % 3 == 0 || i == 0 || j == 0 || j % 4 == 0) {
-                mapa[i][j] = new Celda();
-            }
-            else mapa[i][j] = new Caja();
-            divCelda.classList.add(mapa[i][j].estilo);
-            tablero.appendChild(divCelda);
+    function recogerCajas() {
+        let tablero = document.querySelector('.tablero');
+        for (let i = 0; i < mapa.length; i++) {
+            cajas[i] = [];
+            for (let j = 0; j < mapa[0].length; j++) 
+                cajas[i][j] = tablero.children[i * 21 + j];
         }
     }
-}
 
-// Creación del mapa con las celdas
-var tablero = new Tablero();
-tablero.mapa = inicializarMapa();
+    document.addEventListener('keydown', (key) => {
+        switch (key.key) {
+            case 'ArrowUp':    personaje.move(-1, 0); break;
+            case 'ArrowLeft':  personaje.move(0, -1); break;
+            case 'ArrowRight': personaje.move(0, 1);  break;
+            case 'ArrowDown':  personaje.move(1, 0);
+        }
+    })
 
-// Creacion del personaje
-var personaje = Personaje();
-
-window.addEventListener('load', () => {
-    tablero.mapa[0][0] = new Personaje();
-    pintarTablero(tablero.mapa);
-})
-
-document.addEventListener('keydown', (key) => {
-    console.log(key.key);
-    switch (key.key) {
-        case 'ArrowUp':
-        case 'ArrowLeft':
-        case 'ArrowRight':
-        case 'ArrowDown':
-    }
-    pintarTablero(tablero.mapa);
 })
