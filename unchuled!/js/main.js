10c39338a2869dfd7ba20bf31ccca7e426b0bcdb
MiYazJE <ruben.saizserrano@gmail.com>
2019-10-15 00:34:28 +0200

    La funcion move() ya no es del objeto personaje, ahora es global

--- unchuled!/js/main.js
+++ unchuled!/js/main.js
@@ -9,24 +9,6 @@ window.addEventListener('load', () => {
     let personaje = {};
     personaje.x = 0;
     personaje.y = 0;
-    personaje.move = function (Y, X) {
-        let posX = personaje.x + X;
-        let posY = personaje.y + Y;
-
-        // Salir si la posicion del personaje esta fuera del mapa o si su direccion esta 
-        // ocupada por una caja
-        if (posX == mapa[0].length || posX == -1 || posY == mapa.length || posY == -1) return;
-        if (mapa[posY][posX].classList.contains('caja')) return;
-
-        // Eliminar anterior posicion del personaje
-        mapa[personaje.y][personaje.x].classList.remove('personaje');
-
-        personaje.x = posX;
-        personaje.y = posY;
-
-        // Actualizar la nueva posicion del personaje en el mapa
-        mapa[personaje.y][personaje.x].classList.add('personaje');
-    }
 
     function inicializarMapa() {
 
@@ -53,11 +35,30 @@ window.addEventListener('load', () => {
 
     document.addEventListener('keydown', (key) => {
         switch (key.key) {
-            case 'ArrowUp':    personaje.move(-1, 0); break;
-            case 'ArrowLeft':  personaje.move(0, -1); break;
-            case 'ArrowRight': personaje.move(0, 1);  break;
-            case 'ArrowDown':  personaje.move(1, 0);
+            case 'ArrowUp':    move(-1, 0); break;
+            case 'ArrowLeft':  move(0, -1); break;
+            case 'ArrowRight': move(0, 1);  break;
+            case 'ArrowDown':  move(1, 0);
         }
     })
+
+    function move(Y, X) {
+        let posX = personaje.x + X;
+        let posY = personaje.y + Y;
+
+        // Salir si la posicion del personaje esta fuera del mapa o si su direccion esta 
+        // ocupada por una caja
+        if (posX == mapa[0].length || posX == -1 || posY == mapa.length || posY == -1) return;
+        if (mapa[posY][posX].classList.contains('caja')) return;
+
+        // Eliminar anterior posicion del personaje
+        mapa[personaje.y][personaje.x].classList.remove('personaje');
+
+        personaje.x = posX;
+        personaje.y = posY;
+
+        // Actualizar la nueva posicion del personaje en el mapa
+        mapa[personaje.y][personaje.x].classList.add('personaje');
+    }
 
 })
