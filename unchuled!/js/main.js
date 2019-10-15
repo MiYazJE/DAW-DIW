93769239631b1d036ba28ce309ca18ff4ef0c42c
MiYazJE <ruben.saizserrano@gmail.com>
2019-10-15 03:04:48 +0200

    Imagenes de pisadas creadas e implementadas(4 direcciones)

--- unchuled!/js/main.js
+++ unchuled!/js/main.js
@@ -4,6 +4,9 @@ window.addEventListener('load', () => {
 
     // Creación del mapa con las celdas
     inicializarMapa();
+
+    // De momento el personaje aparecera en la primera celda
+    // TODO mover a otra posicion [0][8]
     mapa[0][0].classList.add('personaje');
 
     let personaje = {};
@@ -20,8 +23,9 @@ window.addEventListener('load', () => {
 
                 let divCelda = document.createElement('div');
 
+                divCelda.classList.add('celda');
                 if (i % 3 == 0 || i == 0 || j == 0 || j % 4 == 0) {
-                    divCelda.classList.add('celda');
+
                 }
                 else {
                     divCelda.classList.add('caja');
@@ -53,11 +57,21 @@ window.addEventListener('load', () => {
 
         // Eliminar anterior posicion del personaje
         mapa[personaje.y][personaje.x].classList.remove('personaje');
+        // Agregamos la clase pisado para que se visualize por donde ha pasado
+
+        let clase = 'pisada-';
+        if (X == 1)  clase += 'derecha';
+        if (X == -1) clase += 'izquierda';
+        if (Y == 1)  clase += 'abajo';
+        if (Y == -1) clase += 'arriba';
+
+        mapa[personaje.y][personaje.x].classList.add(clase);
 
         personaje.x = posX;
         personaje.y = posY;
 
         // Actualizar la nueva posicion del personaje en el mapa
+        mapa[personaje.y][personaje.x].classList = ['celda'];
         mapa[personaje.y][personaje.x].classList.add('personaje');
     }
 
