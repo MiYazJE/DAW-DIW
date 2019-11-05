0ff3cd1cb20c167774b28f8c4bbdf19c13cd4abc
MiYazJE <ruben.saizserrano@gmail.com>
2019-11-05 14:46:15 +0100

    Codigo js a una linea de codigo!

--- UD02/Practicas/Tarea10/js/script.js
+++ UD02/Practicas/Tarea10/js/script.js
@@ -1,24 +1,6 @@
 'use strict';
 
-const setBezierCurve = (caja) => caja.style.transitionTimingFunction = 'cubic-bezier(0, 1.0186, 0.8458, 0.2695)';
+document.querySelector('button').addEventListener('click', () => document.querySelectorAll('.caja').forEach(caja => caja.classList.toggle('mover')));
 
-const insertarPropiedad = (caja) => {
-    let value = caja.classList[caja.classList.length - 1];
-    caja.style.transitionTimingFunction = value;
-}
-
-
-function iniciarTransiciones() {
-    press = !press;
-    cajas.forEach(caja => {
-        let end = (press) ? caja.parentNode.offsetWidth - caja.offsetWidth + "px": "0%";
-        caja.style.transform = `translateX(${end})`;
-    });
-}
-
-let press = false;
-
-const cajas = document.querySelectorAll('.caja');
-cajas.forEach(caja => setBezierCurve(caja));
-
-document.querySelector('button').addEventListener('click', () => iniciarTransiciones());
+// Tarea 11
+// document.querySelector('button').addEventListener('click', () => document.querySelectorAll('.caja').forEach(caja => caja.classList.toggle('bezier')));
