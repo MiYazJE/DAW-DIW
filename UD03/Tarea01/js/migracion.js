bbbf137c06edc55277eecc9772c2a8ec50c3323a
MiYazJE <ruben.saizserrano@gmail.com>
2019-11-19 01:57:02 +0100

    Tarea terminada, posible refactorizacion(promesas)

--- UD03/Tarea01/js/migracion.js
+++ UD03/Tarea01/js/migracion.js
@@ -14,18 +14,46 @@ FUNCIONES PERDIDAS
 
 */
 
-function startMigration(){
+async function startMigration() {
 
-    // Fragmentos perdidos
-    // ^(;,;)^
+    let stepsContainer = document.querySelector('steps').children;
+
+    for (let i = 0; i < stepsContainer.length; i += 3) {
+
+        let steplabel = stepsContainer[i];
+        let progress  = stepsContainer[i + 1];
+        let finalmsg  = stepsContainer[i + 2];
+
+        steplabel.classList.add('estabaEscondido');
+        // Await to the animation ends, then do to the rest
+        await new Promise(resolve => steplabel.addEventListener('transitionend', resolve));
+
+        progress.classList.add('estabaEscondido');
+
+        // Set to the progress bar increments progressively
+        let value = parseInt(progress.getAttribute('value'));
+        let max = parseInt(progress.getAttribute('max'));
+        for (; value <= max; value++) {
+            progress.style.width = value + '%';
+            await sleep(20);
+        }
+        
+        finalmsg.classList.add('estabaEscondido');
+        await new Promise(resolve => finalmsg.addEventListener('transitionend', resolve));
+        finalmsg.classList.add('animacionParpadeo');
+    }
+
 }
 
-function init(){
+// Sleep the runtime
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function init() {
     console.info(" * Init envirnoment ");
 
     // Set click function on button
-    document.querySelector("button").addEventListener("click",startMigration);
+    document.querySelector("button").addEventListener("click", startMigration);
 }
 
 // Init the environment when all is ready
-window.onload=init;
+window.onload = init;
